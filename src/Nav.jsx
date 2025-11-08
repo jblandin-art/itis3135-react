67b19b25d629de06aa76bee22075655d0612bc69
jblandin-art <jblandin@charlotte.edu>
2025-11-07 22:40:25 -0500

    s

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,8 +2,8 @@ export default function() {
     return (
         <nav>
             <a href="./">Home</a> | 
-            <a href="introduction">Introduction</a> | 
-            <a href="contract">Contract</a> | 
+            <a href="#/introduction">Introduction</a> | 
+            <a href="#/contract">Contract</a> | 
         </nav>
     )
 }
