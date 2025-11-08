a2658d0b690762255d8c99a004bea9df1dbe979c
jblandin-art <jblandin@charlotte.edu>
2025-11-07 23:32:47 -0500

    s

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,15 +2,14 @@ import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import { Outlet } from "react-router-dom";
 
-export default function Layout(){
-    return (
-        <>  
-        <Header />
-        
+export default function Layout() {
+  return (
+    <div className="page-container">
+      <Header />
+      <main>
         <Outlet />
-
-        <Footer/>
-        
-        </>
-    )
+      </main>
+      <Footer />
+    </div>
+  );
 }
